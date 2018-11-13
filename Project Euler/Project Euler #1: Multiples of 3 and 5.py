e060dd8fa512e78f014ca4a126a03bca3e5f76e1
Daniel Wu <wuon@protonmail.com>
2018-11-12 22:23:06 -0500

    2

--- Project Euler/Project Euler #1: Multiples of 3 and 5.py
+++ Project Euler/Project Euler #1: Multiples of 3 and 5.py
@@ -3,14 +3,14 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-#!/bin/python3
+# !/bin/python3
 
 import sys
 
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    three = (n-1)//3
-    five = (n-1)//5
-    fifteen = (n-1)//15
-    print(((3*three*(three+1)) >> 1) + ((5*five*(five+1)) >> 1) - ((15*fifteen*(fifteen+1)) >> 1))
+    three = (n - 1) // 3
+    five = (n - 1) // 5
+    fifteen = (n - 1) // 15
+    print(((3 * three * (three + 1)) >> 1) + ((5 * five * (five + 1)) >> 1) - ((15 * fifteen * (fifteen + 1)) >> 1))
