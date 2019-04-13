0ba67fadc47d6ba67b67ae09988300cc97c6578f
Daniel Wu <wuon@protonmail.com>
2019-04-13 11:22:09 -0400

    To Lower Case

---
+++ Leetcode/To Lower Case.py
@@ -0,0 +1,37 @@
+class Solution:
+    def toLowerCase(self, str: 'str') -> 'str':
+        d = {
+            "A": "a",
+            "B": "b",
+            "C": "c",
+            "D": "d",
+            "E": "e",
+            "F": "f",
+            "G": "g",
+            "H": "h",
+            "I": "i",
+            "J": "j",
+            "K": "k",
+            "L": "l",
+            "M": "m",
+            "N": "n",
+            "O": "o",
+            "P": "p",
+            "Q": "q",
+            "R": "r",
+            "S": "s",
+            "T": "t",
+            "U": "u",
+            "V": "v",
+            "W": "w",
+            "X": "x",
+            "Y": "y",
+            "Z": "z",
+        }
+        new = ""
+        for char in str:
+            if char in d:
+                new += d[char]
+            else:
+                new += char
+        return new
