04257a2c82200d8b53b265c686b7887c74e93955
Daniel Wu <wuon@protonmail.com>
2019-10-08 11:04:00 -0400

    Add Binary

---
+++ Leetcode/67.add-binary.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=67 lang=python
+#
+# [67] Add Binary
+#
+
+# @lc code=start
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        o = ""
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            o += str(carry%2)
+            carry //= 2
+        return o[::-1]
+# @lc code=end
